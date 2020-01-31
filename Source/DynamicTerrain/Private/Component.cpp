cbfce6d6582df75c5bc4224c01d0ea9125abd538
UserCodrus <powerinblack@gmail.com>
2020-01-31 13:00:01 -0500

    Removed redundant code from component builder threads

--- Source/DynamicTerrain/Private/Component.cpp
+++ Source/DynamicTerrain/Private/Component.cpp
@@ -5,49 +5,25 @@
 
 #define HMAPMAX 65535
 
-void ComponentData::allocate(uint16 size, uint16 section_id)
+ComponentBuilder::ComponentBuilder(const ATerrain* ParentTerrain)
 {
-	//section = section_id;
+	Terrain = ParentTerrain;
 
-	// Empty all of the arrays
-	vertices.Empty();
-	triangles.Empty();
-	UV0.Empty();
-	normals.Empty();
-	tangents.Empty();
+	ComponentX = 0;
+	ComponentY = 0;
 
-	// Preallocate the arrays
-	vertices.Reserve(size * size);
-	triangles.Reserve((size - 1) * (size - 1) * 6);
-	UV0.Reserve(size * size);
-	normals.Reserve(size * size);
-	tangents.Reserve(size * size);
-}
-
-ComponentBuilder::ComponentBuilder(const UHeightMap* Map, const TArray<FVector>* Normals, const TArray<FProcMeshTangent>* Tangents, float map_height, int32 component_size)
-{
-	heightmap = Map;
-	normalmap = Normals;
-	tangentmap = Tangents;
-	total_height = map_height;
-	component_width = component_size;
-
-	component_x = 0;
-	component_y = 0;
-
-	idle = true;
-	counter;
+	Idle = true;
 
 	// Create the thread
-	thread = FRunnableThread::Create(this, TEXT("ComponentBuilder worker"));
+	Thread = FRunnableThread::Create(this, TEXT("ComponentBuilder worker"));
 }
 
 ComponentBuilder::~ComponentBuilder()
 {
-	if (thread != nullptr)
+	if (Thread != nullptr)
 	{
-		delete thread;
-		thread = nullptr;
+		delete Thread;
+		Thread = nullptr;
 	}
 }
 
@@ -60,61 +36,15 @@ bool ComponentBuilder::Init()
 
 uint32 ComponentBuilder::Run()
 {
-	while (counter.GetValue() == 0)
+	while (Counter.GetValue() == 0)
 	{
-		if (!idle)
+		if (!Idle)
 		{
 			// Build a component section if the thread is not idling
-			data.allocate((unsigned)component_width, 0);
+			Data.Allocate(Terrain->GetComponentSize());
+			Terrain->GenerateMeshSection(ComponentX, ComponentY, Data, true);
 
-			// The number of polygons in each component
-			uint16 polygons = component_width - 1;
-			int32 heightmap_x = heightmap->GetWidthX() - 2;
-			int32 heightmap_y = heightmap->GetWidthY() - 2;
-
-			// The x and y coordinates of the current component in the world
-			float world_x = (float)(heightmap_x - 1) / 2.0f - polygons * component_x;
-			float world_y = (float)(heightmap_y - 1) / 2.0f - polygons * component_y;
-
-			// The x and y coordinates of the current component in the heightmap
-			int32 map_x = component_x * polygons;
-			int32 map_y = component_y * polygons;
-
-			// Create vertex data
-			for (int32 y = 0; y < component_width; ++y)
-			{
-				for (int32 x = 0; x < component_width; ++x)
-				{
-					// Vertices
-					data.vertices.Add(FVector(x - world_x, world_y - y, heightmap->GetHeight(map_x + x + 1, map_y + y + 1) * total_height));
-
-					// Normals
-					data.normals.Add((*normalmap)[(map_y + y) * heightmap_x + (map_x + x)]);
-
-					// Tangents
-					data.tangents.Add((*tangentmap)[(map_y + y) * heightmap_x + (map_x + x)]);
-
-					// UVs
-					data.UV0.Add(FVector2D(x + map_x, y + map_y));
-				}
-			}
-
-			// Create triangles
-			for (int32 y = 0; y < polygons; ++y)
-			{
-				for (int32 x = 0; x < polygons; ++x)
-				{
-					data.triangles.Add(x + (y * component_width));
-					data.triangles.Add(1 + x + y * component_width);
-					data.triangles.Add(1 + x + (y + 1) * component_width);
-
-					data.triangles.Add(x + (y * component_width));
-					data.triangles.Add(1 + x + (y + 1) * component_width);
-					data.triangles.Add(x + (y + 1) * component_width);
-				}
-			}
-
-			idle = true;
+			Idle = true;
 		}
 		else
 		{
@@ -128,21 +58,29 @@ uint32 ComponentBuilder::Run()
 
 void ComponentBuilder::Stop()
 {
-	counter.Increment();
+	Counter.Increment();
 }
 
 /// Builder Interface ///
 
 bool ComponentBuilder::IsIdle()
 {
-	return idle;
+	return Idle;
+}
+
+void ComponentBuilder::Build(int32 component_x, int32 component_y)
+{
+	ComponentX = component_x;
+	ComponentY = component_y;
+
+	Idle = false;
 }
 
 ComponentData* ComponentBuilder::GetData()
 {
-	if (idle)
+	if (Idle)
 	{
-		return &data;
+		return &Data;
 	}
 	else
 	{
@@ -150,13 +88,7 @@ ComponentData* ComponentBuilder::GetData()
 	}
 }
 
-void ComponentBuilder::Build(int32 _component_x, int32 _component_y, uint16 section_number)
+int32 ComponentBuilder::GetSection()
 {
-	component_x = _component_x;
-	component_y = _component_y;
-
-	//data.allocate((unsigned)component_width, section_number);
-	data.section = section_number;
-
-	idle = false;
+	return ComponentY * Terrain->GetXWidth() + ComponentX;
 }
--- Source/DynamicTerrain/Private/Component.h
+++ Source/DynamicTerrain/Private/Component.h
@@ -6,22 +6,10 @@
 
 #include "Terrain.h"
 
-struct ComponentData
-{
-	void allocate(uint16 size, uint16 section_id);
-
-	uint16 section;
-	TArray<FVector> vertices;
-	TArray<int32> triangles;
-	TArray<FVector2D> UV0;
-	TArray<FVector> normals;
-	TArray<FProcMeshTangent> tangents;
-};
-
 class ComponentBuilder : public FRunnable
 {
 public:
-	ComponentBuilder(const UHeightMap* Map, const TArray<FVector>* Normals, const TArray<FProcMeshTangent>* Tangents, float map_height, int32 component_size);
+	ComponentBuilder(const ATerrain* ParentTerrain);
 	~ComponentBuilder();
 
 	/// FRunnable Interface ///
@@ -33,23 +21,20 @@ public:
 	/// Builder Interface ///
 
 	bool IsIdle();
-	void Build(int32 _component_x, int32 _component_y, uint16 section_number);
-	ComponentData* GetData();
+	void Build(int32 component_x, int32 component_y);
 
-	FRunnableThread* thread = nullptr;				// The thread this interface runs on
-	FThreadSafeCounter counter;						// Thread counter for managing the thread
-	ComponentData data;								// The container for the data
+	ComponentData* GetData();
+	int32 GetSection();
+
+	FRunnableThread* Thread = nullptr;				// The thread this interface runs on
+	FThreadSafeCounter Counter;						// Thread counter for managing the thread
+	ComponentData Data;								// The container for the data
 
 private:
-	const UHeightMap* heightmap;					// The heightmap to draw data from
-	const TArray<FVector>* normalmap;				// The vectormap to draw normal data from
-	const TArray<FProcMeshTangent>* tangentmap;		// The vectormap to draw tangent data from
+	const ATerrain* Terrain;						// The parent terrain actor
 
-	float total_height;								// The Heigth of the parent terrain
-	int32 component_width;							// The ComponentSize of the parent terrain
+	int32 ComponentX;
+	int32 ComponentY;
 
-	int32 component_x;
-	int32 component_y;
-
-	bool idle;
+	bool Idle;
 };
