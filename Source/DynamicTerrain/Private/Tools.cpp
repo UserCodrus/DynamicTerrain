49bc2ef49b99cba278f053f8bba3d96bb6a122c7
UserCodrus <powerinblack@gmail.com>
2020-02-04 16:47:36 -0500

    Added a function to find the cursor on a terrain object

--- Source/DynamicTerrain/Private/Tools.cpp
+++ Source/DynamicTerrain/Private/Tools.cpp
@@ -1,6 +1,65 @@
 #include "Tools.h"
 
+#include "Kismet/GameplayStatics.h"
+
 #define LOCTEXT_NAMESPACE "TerrainTools"
+
+float FTerrainTool::TraceDistance = 10000;
+
+void FTerrainTool::Activate()
+{
+	Active = true;
+}
+
+void FTerrainTool::Deactivate()
+{
+	Active = false;
+}
+
+void FTerrainTool::Select(ATerrain* Target)
+{
+	Terrain = Target;
+}
+
+bool FTerrainTool::GetCursorOnTerrain(FHitResult& Result)
+{
+	if (Terrain != nullptr)
+	{
+		UWorld* world = Terrain->GetWorld();
+
+		// Get the location of the mouse in the active game viewport
+		UGameViewportClient* viewport = world->GetGameViewport();
+
+		FVector2D Mouse;
+		if (viewport->GetMousePosition(Mouse))
+		{
+			// Project the mouse into the world
+			FVector WorldOrigin;
+			FVector WorldDirection;
+
+			if (UGameplayStatics::DeprojectScreenToWorld(world->GetFirstPlayerController(), Mouse, WorldOrigin, WorldDirection))
+			{
+				// Trace from the viewport outward under the cursor
+				FHitResult hit;
+				world->LineTraceSingleByChannel(hit, WorldOrigin, WorldOrigin + WorldDirection * TraceDistance, ECollisionChannel::ECC_WorldDynamic);
+
+				AActor* actor = hit.GetActor();
+				if (actor != nullptr)
+				{
+					if (actor->IsA<ATerrain>())
+					{
+						Result = hit;
+						return true;
+					}
+				}
+			}
+		}
+	}
+
+	// Return no result if anything failed
+	Result = FHitResult();
+	return false;
+}
 
 float FTerrainTool::GetMagnitude(float Distance)
 {
@@ -9,7 +68,7 @@ float FTerrainTool::GetMagnitude(float Distance)
 	// Scale exponentially
 	magnitude = (1.0f - magnitude * magnitude);
 	// Apply falloff
-	magnitude = magnitude / Falloff;
+	//magnitude = magnitude / Falloff;
 	// Apply hardness
 	magnitude = magnitude * (1.0f - Hardness) + Hardness;
 	// Clamp the value and apply the strength multiplier
@@ -53,16 +112,6 @@ void FRaiseTool::Use(UHeightMap* Map, FVector2D Center, float Delta)
 	}
 }
 
-void FRaiseTool::Activate()
-{
-
-}
-
-void FRaiseTool::Deactivate()
-{
-
-}
-
 void FRaiseTool::Tick(float DeltaTime)
 {
 	
@@ -71,6 +120,51 @@ void FRaiseTool::Tick(float DeltaTime)
 FText FRaiseTool::GetName()
 {
 	return LOCTEXT("TerrianToolRaiseName", "Raise Terrain");
+}
+
+/// Lower Tool ///
+
+void FLowerTool::Use(UHeightMap* Map, FVector2D Center, float Delta)
+{
+	int32 width_x = Map->GetWidthX();
+	int32 width_y = Map->GetWidthY();
+	if (Center.X < 0 || Center.Y < 0 || Center.X >= width_x || Center.Y >= width_y)
+	{
+		return;
+	}
+
+	// Set the radius of the circle proportional to the size
+	int radius = (int)Size + 1;
+
+	// Raise each vertex within the circle's radius
+	for (int32 x = Center.X - radius; x < Center.X + radius; ++x)
+	{
+		for (int32 y = Center.Y - radius; y < Center.Y + radius; ++y)
+		{
+			if (x >= 0 || y >= 0 || x < width_x || y < width_y)
+			{
+				// Make sure the vertex is within the border of the circle
+				float dist = FVector2D::Distance(Center, FVector2D(x, y));
+
+				if (dist <= Size)
+				{
+					// Raise the vertex height
+					float height = Map->GetHeight(x, y);
+					Map->SetHeight(x, y, height - GetMagnitude(dist) * Delta);
+				}
+			}
+		}
+	}
+}
+
+void FLowerTool::Tick(float DeltaTime)
+{
+
+}
+
+FText FLowerTool::GetName()
+{
+	return LOCTEXT("TerrianToolRaiseName", "Lower Terrain");
 }
 
 #undef LOCTEXT_NAMESPACE
--- Source/DynamicTerrain/Public/Tools.h
+++ Source/DynamicTerrain/Public/Tools.h
@@ -6,24 +6,36 @@
 class DYNAMICTERRAIN_API FTerrainTool
 {
 public:
+	// Use the tool to manipulate a heightmap
 	virtual void Use(UHeightMap* Map, FVector2D Center, float Delta) = 0;
 
-	virtual void Activate() = 0;
-	virtual void Deactivate() = 0;
+	// Retrive the name of the tool for the editor UI
+	virtual FText GetName() = 0;
 
 	virtual void Tick(float DeltaTime) = 0;
 
-	virtual FText GetName() = 0;
+	virtual void Activate();
+	virtual void Deactivate();
 
-	float Size = 10.0f;
-	float Strength = 0.01f;
-	float Falloff = 0.9f;
-	float Hardness = 0.1f;
+	// Select a specific terrain object
+	virtual void Select(ATerrain* Target);
+
+	// Get the location of the mouse cursor on the terrain
+	bool GetCursorOnTerrain(FHitResult& Result);
+
+	float Size = 10.0f;				// The radius of the tool circle
+	float Strength = 0.01f;			// The strength of the tool
+	float Falloff = 0.0f;			// The distance from the center that the strength begins to fall
+	float Hardness = 0.0f;			// The amount that the edges of the cirle contribute
+
+	static float TraceDistance;		// The distance to check for the mouse cursor touching the terrain
 
 protected:
 	virtual float GetMagnitude(float Distance);
 
-	ATerrain* Target = nullptr;
+	ATerrain* Terrain = nullptr;	// The currently selected terrain object
+
+	bool Active = false;			// Set to true when the tool should operate every tick
 };
 
 class DYNAMICTERRAIN_API FRaiseTool : public FTerrainTool
@@ -31,11 +43,17 @@ class DYNAMICTERRAIN_API FRaiseTool : public FTerrainTool
 public:
 	virtual void Use(UHeightMap* Map, FVector2D Center, float Delta) override;
 
-	virtual void Activate() override;
-	virtual void Deactivate() override;
-
 	virtual void Tick(float DeltaTime) override;
 
 	virtual FText GetName() override;
 };
 
+class DYNAMICTERRAIN_API FLowerTool : public FTerrainTool
+{
+public:
+	virtual void Use(UHeightMap* Map, FVector2D Center, float Delta) override;
+
+	virtual void Tick(float DeltaTime) override;
+
+	virtual FText GetName() override;
+};
